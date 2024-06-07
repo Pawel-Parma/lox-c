9c0bcbd5c4817148465fff4cfc0ac8b19e536a83
Pawel-Parma <122152230+Pawel-Parma@users.noreply.github.com>
2024-06-07 21:26:01 +0200

    feat: chapter 14 - chunks of bytecode

---
+++ src/chunk.h
@@ -0,0 +1,26 @@
+#ifndef LOX_C_CHUNK_H
+#define LOX_C_CHUNK_H
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+    OP_CONSTANT,
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    int* lines;
+    uint8_t* code;
+    ValueArray constants;
+} Chunk;
+
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef LOX_C_COMMON_H
+#define LOX_C_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
